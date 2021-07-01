d7ec49d6985655abfb84a3ab81bac15f7b2efbbc
Quentin Gabriele <quentin.gabriele@gmail.com>
2021-07-01 22:07:18 +0200

    add mangopayUser to MangopayPivot

--- src/Models/MangopayPivot.php
+++ src/Models/MangopayPivot.php
@@ -2,7 +2,12 @@
 
 namespace Finller\Mangopay\Models;
 
+use Finller\Mangopay\Exceptions\MangopayUserException;
 use Illuminate\Database\Eloquent\Model;
+use MangoPay\Libraries\Exception;
+use MangoPay\Libraries\ResponseException;
+use MangoPay\MangoPayApi;
+use MangoPay\User;
 
 class MangopayPivot extends Model
 {
@@ -23,5 +28,31 @@ class MangopayPivot extends Model
     public static function findByMangopayId($Id)
     {
         return MangopayPivot::where(['mangopay_id' => $Id])->first();
+    }
+
+    public function mangopayApi(): MangopayApi
+    {
+        return app(MangoPayApi::class);
+    }
+
+    public function mangopayUser(): User
+    {
+        $api = $this->mangopayApi();
+        $userId = $this->mangopay_id;
+        if (!$userId) {
+            throw MangopayUserException::mangopayUserIdNotFound(get_class($this));
+        }
+
+        try {
+            $user = $api->Users->Get($userId);
+        } catch (ResponseException $e) {
+            // handle/log the response exception with code $e->GetCode(), message $e->GetMessage() and error(s) $e->GetErrorDetails()
+            throw $e;
+        } catch (Exception $e) {
+            // handle/log the exception $e->GetMessage()
+            throw $e;
+        }
+
+        return $user;
     }
 }
