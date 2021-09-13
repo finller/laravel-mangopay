8582c5f4082d5b4c1fac4095ebf2fdcfdc60070a
Quentin Gabriele <quentin.gabriele@gmail.com>
2021-09-13 15:03:57 -0400

    fix typo

--- config/mangopay.php
+++ config/mangopay.php
@@ -4,7 +4,7 @@ return [
     'api' => [
         'id' => env('MANGOPAY_ID'),
         'secret' => env('MANGOPAY_KEY'),
-        'url' => env('MANGOPAY_BASE', "https://api.mangopay.com")
+        'url' => env('MANGOPAY_URL', "https://api.mangopay.com")
     ],
     'folder' => '',
     'defaultCurrency' => 'EUR',
